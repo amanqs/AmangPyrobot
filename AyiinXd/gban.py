811236d32620ff630319c623d14404ec9441358a
amanqs <116822442+amanqs@users.noreply.github.com>
2022-12-08 01:23:07 +0700

    Update gban.py

--- AyiinXd/gban.py
+++ AyiinXd/gban.py
@@ -19,93 +19,66 @@ from pyAyiin.pyrogram import eor
 from . import yins
 
 
-@Ayiin(["gban", "gban"])
-async def _(e):
-    xx = await e.eor("`Gbanning...`")
-    reason = ""
-    if e.reply_to_msg_id:
-        userid = (await e.get_reply_message()).sender_id
-        try:
-            reason = e.text.split(" ", maxsplit=1)[1]
-        except IndexError:
-            pass
-    elif e.pattern_match.group(1).strip():
-        usr = e.text.split(maxsplit=2)[1]
-        try:
-            userid = await e.client.parse_id(usr)
-        except ValueError:
-            userid = usr
-        try:
-            reason = e.text.split(maxsplit=2)[2]
-        except IndexError:
-            pass
-    elif e.is_private:
-        userid = e.chat_id
-        try:
-            reason = e.text.split(" ", maxsplit=1)[1]
-        except IndexError:
-            pass
+@Ayiin(["fgcast"])
+async def gban_user(client: Client, message: Message):
+    user_id, reason = await extract_user_and_reason(message, sender_chat=True)
+    if message.from_user.id != client.me.id:
+        AyiinXD = await message.reply("`Gbanning...`")
     else:
-        return await xx.eor("`Reply to some msg or add their id.`", time=5)
-    user = None
-    try:
-        user = await e.client.get_entity(userid)
-        name = inline_mention(user)
-    except BaseException:
-        userid = int(userid)
-        name = str(userid)
-    chats = 0
-    if userid == (await event.client.get_me()).id:
-        return await xx.eor("`I can't gban myself.`", time=3)
-    elif userid in DEVLIST:
-        return await xx.eor("`I can't gban my Developers.`", time=3)
-    elif is_gbanned(userid):
-        return await eod(
-            xx,
-            "`User is already gbanned and added to gbanwatch.`",
-            time=4,
+        AyiinXD = await message.edit("`Gbanning....`")
+    if not user_id:
+        return await AyiinXD.edit("Saya tidak dapat menemukan pengguna itu.")
+    if user_id == client.me.id:
+        return await AyiinXD.edit("**Ngapain NgeGban diri sendiri Goblok 🐽**")
+    if user_id in DEVS:
+        return await AyiinXD.edit("**Gagal GBAN karena dia adalah Pembuat saya 🗿**")
+    if user_id in WHITELIST:
+        return await AyiinXD.edit(
+            "**Gagal GBAN karena dia adalah admin @SharingUserbot 🗿**"
         )
-    if e.client._dialogs:
-        dialog = e.client._dialogs
-    else:
-        dialog = await e.client.get_dialogs()
-        e.client._dialogs.extend(dialog)
-    for ggban in dialog:
-        if ggban.is_group or ggban.is_channel:
-            try:
-                await e.client.edit_permissions(ggban.id, userid, view_messages=False)
-                chats += 1
-            except FloodWaitError as fw:
-                LOGS.info(
-                    f"[FLOOD_WAIT_ERROR] : on GBAN Command\nSleeping for {fw.seconds+10}"
-                )
-                await asyncio.sleep(fw.seconds + 10)
-                try:
-                    await e.client.edit_permissions(
-                        ggban.id, userid, view_messages=False
-                    )
-                    chats += 1
-                except BaseException as er:
-                    LOGS.exception(er)
-            except (ChatAdminRequiredError, ValueError):
-                pass
-            except BaseException as er:
-                LOGS.exception(er)
-    gban(userid, reason)
-    if isinstance(user, User):
-        await e.client(BlockRequest(userid))
-    gb_msg = f"**#Gbanned** {name} `in {chats} chats and added to gbanwatch!`"
+    if user_id:
+        try:
+            user = await client.get_users(user_id)
+        except Exception:
+            return await AyiinXD.edit("`Harap tentukan pengguna yang valid!`")
+
+    if sql.is_gbanned(user.id):
+        return await AyiinXD.edit(
+            f"[Jamet](tg://user?id={user.id}) **ini sudah ada di daftar gbanned**"
+        )
+    f_chats = await get_ub_chats(client)
+    if not f_chats:
+        return await AyiinXD.edit("**Anda tidak mempunyai GC yang anda admin 🥺**")
+    er = 0
+    done = 0
+    for gokid in f_chats:
+        try:
+            await client.ban_chat_member(chat_id=gokid, user_id=int(user.id))
+            done += 1
+        except BaseException:
+            er += 1
+    sql.gban(user.id)
+    msg = (
+        r"**\\#GBanned_User//**"
+        f"\n\n**First Name:** [{user.first_name}](tg://user?id={user.id})"
+        f"\n**User ID:** `{user.id}`"
+    )
     if reason:
-        gb_msg += f"\n**Reason** : {reason}"
-    await xx.edit(gb_msg)
+        msg += f"\n**Reason:** `{reason}`"
+    msg += f"\n**Affected To:** `{done}` **Chats**"
+    await AyiinXD.edit(msg)
+    
+    
+    )
+    
     
     CMD_HELP.update(
     {"gban": (
         "gban",
         {
-            "gbantst <reply>": "Forward Broadcast messages in group chats.",
-            "gtst <reply>": "Broadcast messages in group chats.",
-            "gbncast <reply>": "Broadcast messages in user.",
+            "fgcast <reply>": "Forward Broadcast messages in group chats.",
+            "gcast <reply>": "Broadcast messages in group chats.",
+            "gucast <reply>": "Broadcast messages in user.",
         }
     )
     }
