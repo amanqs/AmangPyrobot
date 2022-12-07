880b95d66c53858a432468efd9179b4a67c59c5c
amanqs <116822442+amanqs@users.noreply.github.com>
2022-12-07 19:49:56 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ load_dotenv(".env")
 class Var:
     API_HASH = getenv("API_HASH")
     API_ID = int(getenv("API_ID", ""))
-    ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/9f8e73d387f25b7f27ce5.jpg")
-    ALIVE_TEXT = getenv("ALIVE_TEXT", "Hey, Saya AyiinUbot Dibuat dengan basis pyrogram versi terbaru")
+    ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/b930d00402db6611bd223.jpg")
+    ALIVE_TEXT = getenv("ALIVE_TEXT", "Hey, Saya AmangPyrobot Dibuat dengan basis pyrogram versi terbaru")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
     BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
     if not BLACKLIST_CHAT:
         BLACKLIST_CHAT = [-1001473548283, -1001675396283]
     LOG_CHAT = int(getenv("LOG_CHAT") or 0)
-    HNDLR = getenv("HNDLR", [".", "!", "*", "^", "-", "?"])
+    HNDLR = getenv("HNDLR", [".", "!", "*", "^", "-", "?", " "])
     DB_URL = getenv("DATABASE_URL", "")
     HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
     HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
